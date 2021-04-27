46021ffc4eb1a2efce5c8af618126e4f62da96dc
dumi994 <dumi.9944@gmail.com>
2021-04-27 16:47:37 +0200

    il programma trasforma i pari e disp. in fizz/buzz

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-var pari = 'buzz';
-var dispari = 'fizz';
+var pari = 'Fizz';
+var dispari = 'Buzz';
+
 
 for (var i = 1; i <= 100; i++){
-    if (i <= 100){
-        console.log(i);
-    } else if(i % 2 == 0){
+    if (i % 3 == 0){
+        console.log(dispari);
+    }else if (i % 5 == 0){
         console.log(pari);
+    }else if(i % 3 == 0 && i % 5 == 0){
+        console.log(pari + dispari);
+    }else{
+        console.log(i);
     }
 }
