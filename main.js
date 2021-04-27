bbd317c1256c195a633cacae831a6b103ecacf1d
dumi994 <dumi.9944@gmail.com>
2021-04-27 16:31:20 +0200

    stampa numeri da 1 a 100

---
+++ main.js
@@ -0,0 +1,10 @@
+var pari = 'buzz';
+var dispari = 'fizz';
+
+for (var i = 1; i <= 100; i++){
+    if (i <= 100){
+        console.log(i);
+    } else if(i % 2 == 0){
+        console.log(pari);
+    }
+}
